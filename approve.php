0300ba9e0acca8d7859b0da5a35d5839eb1bf007
Christiaan van Steenbergen <cvsteenbergen@me.com>
2020-09-22 13:49:42 +0200

    swap closed

---
+++ approve.php
@@ -0,0 +1,92 @@
+<html>
+
+<head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/ethereum/web3.js@1.0.0-beta.34/dist/web3.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+
+<img class="img" style="display:block; margin-left:auto; margin-right:auto; -webkit-filter: blur(0); -webkit-transform: translateZ(0);" src="/img/payaccept-logo-black.png" srcset="/img/payaccept-logo-black@2x.png 2x">
+
+
+<?php
+$realm = 'Restricted area';
+
+//user => password
+$users = array(
+        'admin' => 'admin3333',
+        'guest' => 'guest',
+        'victor' => 'victor123',
+        'mark' => 'buzbee',
+
+
+);
+
+
+if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
+    header('HTTP/1.1 401 Unauthorized');
+    header('WWW-Authenticate: Digest realm="'.$realm.
+        '",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
+
+    die('You are not allowed to login!');
+}
+
+
+// analyze the PHP_AUTH_DIGEST variable
+if (!($data = http_digest_parse($_SERVER['PHP_AUTH_DIGEST'])) ||
+    !isset($users[$data['username']]))
+    die('Wrong Credentials!');
+
+// generate the valid response
+$A1 = md5($data['username'] . ':' . $realm . ':' . $users[$data['username']]);
+$A2 = md5($_SERVER['REQUEST_METHOD'].':'.$data['uri']);
+$valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.$A2);
+
+if ($data['response'] != $valid_response)
+    die('Wrong Credentials!');
+
+// ok, valid username & password
+//echo 'Welcome, ' . $data['username'];
+mail("info@payaccept.net","This user did the swap ", $data['username']);
+
+// function to parse the http auth header
+function http_digest_parse($txt)
+{
+    // protect against missing data
+    $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
+    $data = array();
+    $keys = implode('|', array_keys($needed_parts));
+
+    preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $txt, $matches, PREG_SET_ORDER);
+
+    foreach ($matches as $m) {
+        $data[$m[1]] = $m[3] ? $m[3] : $m[4];
+        unset($needed_parts[$m[1]]);
+    }
+
+    return $needed_parts ? false : $data;
+}
+
+
+
+?>
+    <section class="wrapper">
+        <div class="container" id="loaderDiv">
+            <p class="text" >
+                    <i class="fas fa-spinner fa-spin fa-3x"></i>
+                    </br>
+                    </br>
+                    Loading
+                </p>
+        </div>
+    </section>
+
+</body>
+<script src="./payAccept.js"></script>
+<script src="./metaMask.js"></script>
+</html>
